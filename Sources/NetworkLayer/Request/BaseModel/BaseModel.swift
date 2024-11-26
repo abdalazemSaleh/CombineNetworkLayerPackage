c63f828106fe3bada8377d23e3df1933e65b9fa8
Abdalazem Saleh <abdalazem.saleh2002@gmail.com>
2024-11-26 11:47:23 +0200

    change status type

--- Sources/NetworkLayer/Request/BaseModel/BaseModel.swift
+++ Sources/NetworkLayer/Request/BaseModel/BaseModel.swift
@@ -8,7 +8,19 @@
 import Foundation
 
 public struct BaseModel<T: Codable>: Codable {
-    public let status: Bool?
+    public let status: Status?
     public let message: String?
     public let data: T?
+}
+
+public enum Status: String, Codable {
+    case success = "success"
+    case fail = "fail"
+    case unknown
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        let value = try? container.decode(String.self)
+        self = Status(rawValue: value ?? "") ?? .unknown
+    }
 }
