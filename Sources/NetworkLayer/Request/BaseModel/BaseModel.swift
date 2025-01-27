429b93eaf582b59df422d5c670504dbbaeb2067f
Abdalazem Saleh <abdalazem.saleh2002@gmail.com>
2025-01-27 15:19:19 +0200

    Add pagination to base model

--- Sources/NetworkLayer/Request/BaseModel/BaseModel.swift
+++ Sources/NetworkLayer/Request/BaseModel/BaseModel.swift
@@ -10,6 +10,7 @@ import Foundation
 public struct BaseModel<T: Codable>: Codable {
     public let status: Status?
     public let message: String?
+    public let meta: PagenationModel?
     public let data: T?
 }
 
@@ -22,5 +23,23 @@ public enum Status: String, Codable {
         let container = try decoder.singleValueContainer()
         let value = try? container.decode(String.self)
         self = Status(rawValue: value ?? "") ?? .unknown
+    }
+}
+
+public struct PagenationModel: Codable {
+    let currentPage: Int?
+    let from: Int?
+    let lastPage: Int?
+    let perPage: Int?
+    let to: Int?
+    let total: Int?
+    
+    enum CodingKeys: String, CodingKey {
+        case currentPage = "current_page"
+        case from
+        case lastPage = "last_page"
+        case perPage = "per_page"
+        case to
+        case total
     }
 }
